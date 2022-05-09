6583ae6de26f154be478d8b707636d6e61d89255
MorpheouS72 <nazimbeggour@gmail.com>
2022-05-09 23:50:57 +0100

    MaJ Structure de l'app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route} from "react-router-dom"
+import About from './pages/About';
+import Home from './pages/Home';
 
 const App = () => {
   return (
     <div>
-      <h1>Hello React</h1>
-      <div className="title"></div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element ={<Home />} />
+        <Route path="/about" element ={<About />} />        
+        <Route path="*" element ={<Home />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 };
---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, quam explicabo quia voluptate perferendis consectetur laborum ea velit iusto fugiat perspiciatis earum optio? Minus, corrupti enim assumenda maiores quia sit!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam nihil quibusdam aliquam. Minus totam, nihil nemo eius, officiis explicabo atque eligendi ducimus eum adipisci voluptatum unde consequuntur. Rem, numquam? Quis!
+      </p>
+      <br />
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi ipsum aliquid maiores! Nam eveniet magni alias fugit nemo cumque doloremque dicta, non dolore molestias voluptatem quam quod sed autem ullam quo, sunt itaque fugiat modi. Voluptatum tempore eaque ratione, enim sequi vero explicabo quia necessitatibus ipsa repudiandae at nesciunt adipisci possimus ullam beatae voluptates exercitationem non quod mollitia, amet reprehenderit similique. Aperiam a alias ducimus similique totam, corporis corrupti excepturi, porro eos voluptas ratione nisi atque? Aliquid velit dicta nam fugit ducimus nemo in tempore harum consequatur, eius quas. Iusto repudiandae adipisci obcaecati, blanditiis in, error doloribus sint porro delectus nesciunt natus saepe, earum voluptatum quo animi recusandae eius fugiat! Dolor, quos fugiat. Doloremque exercitationem harum repellat, ipsam voluptatibus sed dignissimos quibusdam, maxime facilis neque vero illo. Non nihil recusandae quo expedita incidunt dolor temporibus, excepturi accusamus officiis voluptatem accusantium ducimus nobis? Eveniet, iste a. Veniam deserunt suscipit, eveniet voluptatibus illo maiores ipsam quidem aspernatur possimus itaque saepe voluptate nemo odio atque ullam consequuntur sapiente dolorem quod numquam molestiae! Debitis fugit explicabo corrupti commodi minima. Officiis porro esse accusamus in! Ex sed dicta molestias a delectus harum? Accusantium explicabo sint odio expedita impedit, ipsam modi consequatur eius odit nobis unde.</p>
+    </div>
+  );
+};
+
+export default About;
